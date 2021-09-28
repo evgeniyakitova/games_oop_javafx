eab3ac2687f21c4dc0d7a6234d7c07aad20ee17d
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2021-09-28 22:50:09 +0300

    2. Сокобан. Тестовое задание. [#75283] Исправление

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,7 +4,7 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 1 && checkLine(board, i) || checkColumn(board, i)) {
+            if (board[i][i] == 1 && (checkLine(board, i) || checkColumn(board, i))) {
                 rsl = true;
                 break;
             }
